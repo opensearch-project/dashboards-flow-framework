7a76de1ef947d7706d4ef4e99b5f131536b22673
Tyler Ohlsen <ohltyler@amazon.com>
2023-09-05 12:21:56 -0700

    Add lint script, workflow, and pre-commit hook
    Signed-off-by: Tyler Ohlsen <ohltyler@amazon.com>

--- common/constants.ts
+++ common/constants.ts
@@ -2,6 +2,7 @@
  * Copyright OpenSearch Contributors
  * SPDX-License-Identifier: Apache-2.0
  */
+
 export const PLUGIN_ID = 'aiFlowDashboards';
 
 export const BASE_NODE_API_PATH = '/api/ai_flow';
