24c42a6eb400cd3535ef3f5a8f1ce07e10af20ab
Tyler Ohlsen <ohltyler@amazon.com>
2023-09-21 14:30:54 -0700

    Set up redux infrastructure (#33)
    Signed-off-by: Tyler Ohlsen <ohltyler@amazon.com>
    (cherry picked from commit c73ffb00e0a65d7021fd918544224c448b95d0d0)

--- public/render_app.tsx
+++ public/render_app.tsx
@@ -6,17 +6,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { Provider } from 'react-redux';
 import { AppMountParameters, CoreStart } from '../../../src/core/public';
 import { AiFlowDashboardsApp } from './app';
+import { store } from './store';
 
 export const renderApp = (
   coreStart: CoreStart,
   { appBasePath, element }: AppMountParameters
 ) => {
   ReactDOM.render(
-    <Router basename={appBasePath + '#/'}>
-      <Route render={(props) => <AiFlowDashboardsApp {...props} />} />
-    </Router>,
+    <Provider store={store}>
+      <Router basename={appBasePath + '#/'}>
+        <Route render={(props) => <AiFlowDashboardsApp {...props} />} />
+      </Router>
+    </Provider>,
     element
   );
 
