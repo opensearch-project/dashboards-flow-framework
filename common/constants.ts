e474349dfed9f7e965684f7785ba582cf604e990
Tyler Ohlsen <ohltyler@amazon.com>
2023-09-22 16:26:36 -0700

    Add redux thunk fns; add cat indices API (#37)
    * Add redux thunk fns; add cat indices API
    
    Signed-off-by: Tyler Ohlsen <ohltyler@amazon.com>
    
    * Remove unnecessary setIndices()
    
    Signed-off-by: Tyler Ohlsen <ohltyler@amazon.com>
    
    ---------
    
    Signed-off-by: Tyler Ohlsen <ohltyler@amazon.com>
    (cherry picked from commit 8a171f7f911910daf1a0f92bb8677ea1e6578d02)

--- common/constants.ts
+++ common/constants.ts
@@ -5,4 +5,6 @@
 export const PLUGIN_ID = 'aiFlowDashboards';
 
 export const BASE_NODE_API_PATH = '/api/ai_flow';
-export const SEARCH_PATH = `${BASE_NODE_API_PATH}/search`;
+export const BASE_INDICES_NODE_API_PATH = `${BASE_NODE_API_PATH}/indices`;
+export const SEARCH_INDICES_PATH = `${BASE_INDICES_NODE_API_PATH}/search`;
+export const FETCH_INDICES_PATH = `${BASE_INDICES_NODE_API_PATH}/fetch`;
---
+++ public/store/reducers/opensearch_reducer.ts
@@ -0,0 +1,53 @@
+/*
+ * Copyright OpenSearch Contributors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+import { getRouteService } from '../../services';
+import { IIndex } from '../../../common';
+
+const initialState = {
+  loading: false,
+  errorMessage: '',
+  indices: {} as { [key: string]: IIndex },
+};
+
+const OPENSEARCH_PREFIX = 'opensearch';
+const FETCH_INDICES_ACTION = `${OPENSEARCH_PREFIX}/fetchIndices`;
+
+export const fetchIndices = createAsyncThunk(
+  FETCH_INDICES_ACTION,
+  async (pattern?: string) => {
+    // defaulting to fetch everything except system indices (starting with '.')
+    const patternString = pattern || '*,-.*';
+    const response = getRouteService().fetchIndices(patternString);
+    return response;
+  }
+);
+
+const opensearchSlice = createSlice({
+  name: OPENSEARCH_PREFIX,
+  initialState,
+  reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchIndices.pending, (state, action) => {
+        state.loading = true;
+      })
+      .addCase(fetchIndices.fulfilled, (state, action) => {
+        const indicesMap = new Map<string, IIndex>();
+        action.payload.forEach((index: IIndex) => {
+          indicesMap.set(index.name, index);
+        });
+        state.indices = Object.fromEntries(indicesMap.entries());
+        state.loading = false;
+      })
+      .addCase(fetchIndices.rejected, (state, action) => {
+        state.errorMessage = action.payload as string;
+        state.loading = false;
+      });
+  },
+});
+
+export const opensearchReducer = opensearchSlice.reducer;
