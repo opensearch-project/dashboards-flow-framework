42a6fa6fa79d493e27203a6a4f80a88bc6ffbc04
Tyler Ohlsen <ohltyler@amazon.com>
2023-09-25 12:57:13 -0700

    Refactor into existing Workspace component
    Signed-off-by: Tyler Ohlsen <ohltyler@amazon.com>

--- public/pages/workflow_detail/workspace/workspace.tsx
+++ public/pages/workflow_detail/workspace/workspace.tsx
@@ -3,24 +3,161 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-import React from 'react';
+import React, { useRef, useContext, useCallback, useEffect } from 'react';
+import ReactFlow, {
+  Controls,
+  Background,
+  useNodesState,
+  useEdgesState,
+  addEdge,
+} from 'reactflow';
 import { useSelector } from 'react-redux';
-import { EuiFlexGroup, EuiFlexItem } from '@elastic/eui';
-import { AppState } from '../../../store';
-import { WorkspaceComponent } from '../workspace_component';
+import { EuiFlexItem, EuiFlexGroup } from '@elastic/eui';
+import { AppState, rfContext } from '../../../store';
 
-export function Workspace() {
-  const { components } = useSelector((state: AppState) => state.workspace);
+// styling
+import 'reactflow/dist/style.css';
+import './reactflow-styles.scss';
+import { convertToReactFlowData } from '../../../../common';
+
+// eslint-disable-next-line @typescript-eslint/no-empty-interface
+interface WorkspaceProps {}
+
+export function Workspace(props: WorkspaceProps) {
+  const reactFlowWrapper = useRef(null);
+  const { reactFlowInstance, setReactFlowInstance } = useContext(rfContext);
+
+  // Fetching workspace state to populate the initial nodes/edges.
+  // Where/how the low-level ReactFlow JSON will be persisted is TBD.
+  // TODO: update when that design is finalized
+  const storedComponents = useSelector(
+    (state: AppState) => state.workspace.components
+  );
+  const { rfNodes, rfEdges } = convertToReactFlowData(storedComponents);
+  const [nodes, setNodes, onNodesChange] = useNodesState(rfNodes);
+  const [edges, setEdges, onEdgesChange] = useEdgesState(rfEdges);
+
+  const onConnect = useCallback(
+    (params) => {
+      setEdges((eds) => addEdge(params, eds));
+    },
+    // TODO: add customized logic to prevent connections based on the node's
+    // allowed inputs. If allowed, update that node state as well with the added
+    // connection details.
+    [setEdges]
+  );
+
+  const onDragOver = useCallback((event) => {
+    event.preventDefault();
+    event.dataTransfer.dropEffect = 'move';
+  }, []);
+
+  const onDrop = useCallback(
+    (event) => {
+      event.preventDefault();
+      // Get the node info from the event metadata
+      const nodeData = event.dataTransfer.getData('application/reactflow');
+
+      // check if the dropped element is valid
+      if (typeof nodeData === 'undefined' || !nodeData) {
+        return;
+      }
+
+      // Fetch bounds based on the ref'd div component, adjust as needed.
+      // TODO: remove hardcoded bounds and fetch from a constant somewhere
+      // @ts-ignore
+      const reactFlowBounds = reactFlowWrapper.current.getBoundingClientRect();
+      // @ts-ignore
+      const position = reactFlowInstance.project({
+        x: event.clientX - reactFlowBounds.left - 80,
+        y: event.clientY - reactFlowBounds.top - 90,
+      });
+
+      // TODO: remove hardcoded values when more component info is passed in the event.
+      // Only keep the calculated 'positioning' field.
+      const newNode = {
+        // TODO: generate ID based on the node data maybe
+        id: Date.now().toFixed(),
+        type: nodeData.type,
+        position,
+        data: { label: nodeData.label },
+        style: {
+          background: 'white',
+        },
+      };
+
+      // TODO: add logic to add node into the redux datastore
+
+      setNodes((nds) => nds.concat(newNode));
+    },
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    [reactFlowInstance]
+  );
+
+  // Initialization hook
+  useEffect(() => {
+    // TODO: fetch the nodes/edges dynamically (loading existing flow,
+    // creating fresh from template, creating blank template, etc.)
+    // Will involve populating and/or fetching from redux store
+  }, []);
 
   return (
-    <EuiFlexGroup direction="row">
-      {components.map((component, idx) => {
-        return (
-          <EuiFlexItem key={idx}>
-            <WorkspaceComponent component={component} />
-          </EuiFlexItem>
-        );
-      })}
-    </EuiFlexGroup>
+    <EuiFlexItem grow={true}>
+      <EuiFlexGroup
+        direction="column"
+        gutterSize="m"
+        justifyContent="spaceBetween"
+        className="workspace"
+      >
+        <EuiFlexItem
+          style={{
+            borderStyle: 'groove',
+            borderColor: 'gray',
+            borderWidth: '1px',
+          }}
+        >
+          {/**
+           * We have these wrapper divs & reactFlowWrapper ref to control and calculate the
+           * ReactFlow bounds when calculating node positioning.
+           */}
+          <div className="reactflow-parent-wrapper">
+            <div className="reactflow-wrapper" ref={reactFlowWrapper}>
+              <ReactFlow
+                nodes={nodes}
+                edges={edges}
+                onNodesChange={onNodesChange}
+                onEdgesChange={onEdgesChange}
+                onConnect={onConnect}
+                onInit={setReactFlowInstance}
+                onDrop={onDrop}
+                onDragOver={onDragOver}
+                fitView
+              >
+                <Controls />
+                <Background />
+              </ReactFlow>
+            </div>
+          </div>
+        </EuiFlexItem>
+      </EuiFlexGroup>
+    </EuiFlexItem>
   );
 }
+
+// TODO: remove later, leaving for reference
+
+// export function Workspace() {
+//   const { components } = useSelector((state: AppState) => state.workspace);
+
+//   return (
+//     <EuiFlexGroup direction="row">
+//       {components.map((component, idx) => {
+//         return (
+//           <EuiFlexItem key={idx}>
+//             <WorkspaceComponent component={component} />
+//           </EuiFlexItem>
+//         );
+//       })}
+//     </EuiFlexGroup>
+//   );
+// }
--- public/store/reducers/workspace_reducer.ts
+++ public/store/reducers/workspace_reducer.ts
@@ -4,77 +4,18 @@
  */
 
 import { createSlice } from '@reduxjs/toolkit';
-import { Node, Edge } from 'reactflow';
 import { IComponent } from '../../../common';
 import { KnnIndex, TextEmbeddingProcessor } from '../../component_types';
 
-// TODO: fetch from server-size if it is a created workflow, else have some default
-// mapping somewhere (e.g., 'semantic search': text_embedding_processor, knn_index, etc.)
-
-// TODO: we should store as IComponents. Have some helper fn for converting these to a
-// actual ReactFlow Node. examples of reactflow nodes below.
-const iComponents = [
+// TODO: should be fetched from server-side
+const dummyComponents = [
   new TextEmbeddingProcessor(),
   new KnnIndex(),
 ] as IComponent[];
 
-const dummyComponents = [
-  {
-    id: 'semantic-search',
-    position: { x: 40, y: 10 },
-    data: { label: 'Semantic Search' },
-    type: 'group',
-    style: {
-      height: 110,
-      width: 700,
-    },
-  },
-  {
-    id: 'model',
-    position: { x: 25, y: 25 },
-    data: { label: 'Deployed Model ID' },
-    type: 'default',
-    parentNode: 'semantic-search',
-    extent: 'parent',
-  },
-  {
-    id: 'ingest-pipeline',
-    position: { x: 262, y: 25 },
-    data: { label: 'Ingest Pipeline Name' },
-    type: 'default',
-    parentNode: 'semantic-search',
-    extent: 'parent',
-  },
-] as Array<
-  Node<
-    {
-      label: string;
-    },
-    string | undefined
-  >
->;
-
-const dummyEdges = [
-  {
-    id: 'e1-2',
-    source: 'model',
-    target: 'ingest-pipeline',
-    style: {
-      strokeWidth: 2,
-      stroke: 'black',
-    },
-    markerEnd: {
-      type: 'arrow',
-      strokeWidth: 1,
-      color: 'black',
-    },
-  },
-] as Array<Edge<any>>;
-
 const initialState = {
   isDirty: false,
   components: dummyComponents,
-  edges: dummyEdges,
 };
 
 const workspaceSlice = createSlice({
@@ -91,17 +32,8 @@ const workspaceSlice = createSlice({
       state.components = action.payload;
       state.isDirty = true;
     },
-    setEdges(state, action) {
-      state.edges = action.payload;
-      state.isDirty = true;
-    },
   },
 });
 
 export const workspaceReducer = workspaceSlice.reducer;
-export const {
-  setDirty,
-  removeDirty,
-  setComponents,
-  setEdges,
-} = workspaceSlice.actions;
+export const { setDirty, removeDirty, setComponents } = workspaceSlice.actions;
