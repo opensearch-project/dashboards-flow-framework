ab0f4df835604aa88f4dfc94a9e2ebcb8d5e1daf
Tyler Ohlsen <ohltyler@amazon.com>
2023-10-06 16:46:37 -0700

    Handle toggling state
    Signed-off-by: Tyler Ohlsen <ohltyler@amazon.com>

--- public/pages/workflow_detail/workspace/component_inputs.tsx
+++ public/pages/workflow_detail/workspace/component_inputs.tsx
@@ -11,15 +11,20 @@ import {
   EuiSpacer,
   EuiPanel,
   EuiTitle,
+  EuiEmptyPrompt,
+  EuiText,
 } from '@elastic/eui';
-import { ReactFlowComponent, ReactFlowEdge } from '../../../../common';
+import { ReactFlowComponent } from '../../../../common';
 import { rfContext } from '../../../store';
 import { InputFieldList } from '../workspace_component/input_field_list';
 
 // styling
 import './workspace-styles.scss';
 
-interface ComponentInputsProps {}
+interface ComponentInputsProps {
+  onToggleChange: () => void;
+  isOpen: boolean;
+}
 
 export function ComponentInputs(props: ComponentInputsProps) {
   // TODO: use this instance to update the internal node state. ex: update field data in the selected node based
@@ -30,12 +35,18 @@ export function ComponentInputs(props: ComponentInputsProps) {
     ReactFlowComponent
   >();
 
-  // using custom hook provided by ReactFlow to populate the selected
-  // workspace component
+  /**
+   * Hook provided by reactflow to listen on when nodes are selected / de-selected.
+   * - populate panel content appropriately
+   * - open the panel if a node is selected and the panel is closed
+   */
   useOnSelectionChange({
     onChange: ({ nodes, edges }) => {
       if (nodes && nodes.length > 0) {
         setSelectedComponent(nodes[0]);
+        if (!props.isOpen) {
+          props.onToggleChange();
+        }
       } else {
         setSelectedComponent(undefined);
       }
@@ -43,18 +54,38 @@ export function ComponentInputs(props: ComponentInputsProps) {
   });
 
   return (
-    <EuiFlexGroup direction="column" gutterSize="m" className="workspace-panel">
+    <EuiFlexGroup
+      direction="column"
+      gutterSize="none"
+      className="workspace-panel"
+    >
       <EuiFlexItem className="resizable-panel-border">
-        <EuiPanel paddingSize="s">
-          <>
-            <EuiTitle size="m">
-              <h2>{selectedComponent?.data.label || ''}</h2>
-            </EuiTitle>
-            <EuiSpacer size="s" />
-            <InputFieldList
-              inputFields={selectedComponent?.data.fields || []}
+        <EuiPanel paddingSize="m">
+          {selectedComponent ? (
+            <>
+              <EuiTitle size="m">
+                <h2>{selectedComponent?.data.label || ''}</h2>
+              </EuiTitle>
+              <EuiSpacer size="s" />
+              <InputFieldList
+                inputFields={selectedComponent?.data.fields || []}
+              />
+            </>
+          ) : (
+            <EuiEmptyPrompt
+              iconType={'iInCircle'}
+              title={<h2>No component selected</h2>}
+              titleSize="s"
+              body={
+                <>
+                  <EuiText>
+                    Add a component, or select a component to view or edit its
+                    configuration.
+                  </EuiText>
+                </>
+              }
             />
-          </>
+          )}
         </EuiPanel>
       </EuiFlexItem>
     </EuiFlexGroup>
--- public/pages/workflow_detail/workspace/resizable_workspace.tsx
+++ public/pages/workflow_detail/workspace/resizable_workspace.tsx
@@ -3,7 +3,7 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-import React from 'react';
+import React, { useRef, useState } from 'react';
 import { ReactFlowProvider } from 'reactflow';
 import { EuiResizableContainer } from '@elastic/eui';
 import { Workflow } from '../../../../common';
@@ -15,27 +15,48 @@ interface ResizableWorkspaceProps {
 }
 
 export function ResizableWorkspace(props: ResizableWorkspaceProps) {
+  const [isOpen, setIsOpen] = useState<boolean>(true);
+  const collapseFn = useRef(
+    (id: string, options: { direction: 'left' | 'right' }) => {}
+  );
+
+  const onToggleChange = () => {
+    collapseFn.current('inputsPanel', { direction: 'left' });
+    setIsOpen(!isOpen);
+  };
+
   return (
     <EuiResizableContainer
       direction="horizontal"
       style={{ marginLeft: '-14px' }}
     >
-      {(EuiResizablePanel, EuiResizableButton) => (
-        <ReactFlowProvider>
-          <EuiResizablePanel
-            mode="main"
-            initialSize={60}
-            minSize="50%"
-            style={{ margin: 0, padding: 0 }}
-          >
-            <Workspace workflow={props.workflow} />
-          </EuiResizablePanel>
-          <EuiResizableButton />
-          <EuiResizablePanel mode="collapsible" initialSize={40} minSize="10%">
-            <ComponentInputs />
-          </EuiResizablePanel>
-        </ReactFlowProvider>
-      )}
+      {(EuiResizablePanel, EuiResizableButton, { togglePanel }) => {
+        if (togglePanel) {
+          collapseFn.current = (panelId: string, { direction }) =>
+            togglePanel(panelId, { direction });
+        }
+
+        return (
+          <ReactFlowProvider>
+            <EuiResizablePanel mode="main" initialSize={75} minSize="50%">
+              <Workspace workflow={props.workflow} />
+            </EuiResizablePanel>
+            <EuiResizableButton />
+            <EuiResizablePanel
+              id="inputsPanel"
+              mode="collapsible"
+              initialSize={25}
+              minSize="10%"
+              onToggleCollapsedInternal={() => onToggleChange()}
+            >
+              <ComponentInputs
+                onToggleChange={onToggleChange}
+                isOpen={isOpen}
+              />
+            </EuiResizablePanel>
+          </ReactFlowProvider>
+        );
+      }}
     </EuiResizableContainer>
   );
 }
