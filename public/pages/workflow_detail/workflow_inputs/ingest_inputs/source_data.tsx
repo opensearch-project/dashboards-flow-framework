524875ac64d395b87893e04993bedc86b74f79f8
Tyler Ohlsen <ohltyler@amazon.com>
2024-06-28 09:45:42 -0700

    Add invalid workflow state; support JSON doc uploads (#200)
    Signed-off-by: Tyler Ohlsen <ohltyler@amazon.com>
    (cherry picked from commit 2ef7e05902396911ad308c16a2efbddfe177ccb4)

--- public/pages/workflow_detail/workflow_inputs/ingest_inputs/source_data.tsx
+++ public/pages/workflow_detail/workflow_inputs/ingest_inputs/source_data.tsx
@@ -5,7 +5,12 @@
 
 import React, { useEffect } from 'react';
 import { useFormikContext } from 'formik';
-import { EuiFlexGroup, EuiFlexItem, EuiTitle } from '@elastic/eui';
+import {
+  EuiFilePicker,
+  EuiFlexGroup,
+  EuiFlexItem,
+  EuiTitle,
+} from '@elastic/eui';
 import { JsonField } from '../input_fields';
 import { IConfigField, WorkspaceFormValues } from '../../../../../common';
 
@@ -18,7 +23,15 @@ interface SourceDataProps {
  * Input component for configuring the source data for ingest.
  */
 export function SourceData(props: SourceDataProps) {
-  const { values } = useFormikContext<WorkspaceFormValues>();
+  const { values, setFieldValue } = useFormikContext<WorkspaceFormValues>();
+
+  // files state. when a file is read, update the form value.
+  const fileReader = new FileReader();
+  fileReader.onload = (e) => {
+    if (e.target) {
+      setFieldValue('ingest.docs', e.target.result);
+    }
+  };
 
   // Hook to listen when the docs form value changes.
   // Try to set the ingestDocs if possible
@@ -34,6 +47,19 @@ export function SourceData(props: SourceDataProps) {
         <EuiTitle size="s">
           <h2>Source data</h2>
         </EuiTitle>
+      </EuiFlexItem>
+      <EuiFlexItem grow={false}>
+        <EuiFilePicker
+          accept="application/json"
+          multiple={false}
+          initialPromptText="Select a JSON file containing documents"
+          onChange={(files) => {
+            if (files && files.length > 0) {
+              fileReader.readAsText(files[0]);
+            }
+          }}
+          display="default"
+        />
       </EuiFlexItem>
       <EuiFlexItem grow={false}>
         <JsonField
