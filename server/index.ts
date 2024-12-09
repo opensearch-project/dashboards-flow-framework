ff28662d74eb98f779c1d84fb5b87c321f2a9d78
Sai Medhini Reddy Maryada <117196660+saimedhi@users.noreply.github.com>
2024-12-09 09:29:47 -0800

    Adding feature flag to OpenSearch Flow (#517)
    Signed-off-by: saimedhi <saimedhi@amazon.com>

--- server/index.ts
+++ server/index.ts
@@ -3,8 +3,16 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-import { PluginInitializerContext } from '../../../src/core/server';
+import {
+  PluginConfigDescriptor,
+  PluginInitializerContext,
+} from '../../../src/core/server';
 import { FlowFrameworkDashboardsPlugin } from './plugin';
+import { configSchema, ConfigSchema } from '../server/types';
+
+export const config: PluginConfigDescriptor<ConfigSchema> = {
+  schema: configSchema,
+};
 
 // This exports static code and TypeScript types,
 // as well as, OpenSearch Dashboards Platform `plugin()` initializer.
