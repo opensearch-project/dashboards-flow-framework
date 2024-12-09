06f464b82617e1de7b43933671480b8d94638d8b
saimedhi <saimedhi@amazon.com>
2024-12-09 06:39:39 -0800

    Adding feature flag to OpenSearch Flow
    Signed-off-by: saimedhi <saimedhi@amazon.com>

--- server/types.ts
+++ server/types.ts
@@ -3,5 +3,13 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
+import { schema, TypeOf } from '@osd/config-schema';
+
+export const configSchema = schema.object({
+  enabled: schema.maybe(schema.boolean()),
+});
+
+export type ConfigSchema = TypeOf<typeof configSchema>;
+
 export interface FlowFrameworkDashboardsPluginSetup {}
 export interface FlowFrameworkDashboardsPluginStart {}
