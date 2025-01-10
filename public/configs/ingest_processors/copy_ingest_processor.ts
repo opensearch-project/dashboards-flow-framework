2f841c2ebf558554d5b910a99b555f1e93d4d9da
Tyler Ohlsen <ohltyler@amazon.com>
2025-01-09 16:21:56 -0800

    Onboard copy ingest processor
    Signed-off-by: Tyler Ohlsen <ohltyler@amazon.com>

---
+++ public/configs/ingest_processors/copy_ingest_processor.ts
@@ -0,0 +1,60 @@
+/*
+ * Copyright OpenSearch Contributors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+import { PROCESSOR_TYPE } from '../../../common';
+import { generateId } from '../../utils';
+import { Processor } from '../processor';
+
+/**
+ * The copy ingest processor
+ */
+export class CopyIngestProcessor extends Processor {
+  constructor() {
+    super();
+    this.name = 'Copy Processor';
+    this.type = PROCESSOR_TYPE.COPY;
+    this.id = generateId('copy_processor_ingest');
+    this.fields = [
+      {
+        id: 'source_field',
+        type: 'string',
+      },
+      {
+        id: 'target_field',
+        type: 'string',
+      },
+    ];
+    this.optionalFields = [
+      {
+        id: 'ignore_missing',
+        type: 'boolean',
+        value: false,
+      },
+      {
+        id: 'override_target',
+        type: 'boolean',
+        value: false,
+      },
+      {
+        id: 'remove_source',
+        type: 'boolean',
+        value: false,
+      },
+      {
+        id: 'ignore_failure',
+        type: 'boolean',
+        value: false,
+      },
+      {
+        id: 'description',
+        type: 'string',
+      },
+      {
+        id: 'tag',
+        type: 'string',
+      },
+    ];
+  }
+}
