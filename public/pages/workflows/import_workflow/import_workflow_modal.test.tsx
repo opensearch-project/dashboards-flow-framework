38499926cf9307aed7a6e0c4bfcaac75b283907b
Tyler Ohlsen <ohltyler@amazon.com>
2026-04-06 10:05:04 -0700

    Revert "Fix flaky tests by replacing singleton store with mock store"
    This reverts commit 803924b19e785bb0ed99b119d60644ed48941797.
    
    Signed-off-by: Tyler Ohlsen <ohltyler@amazon.com>

--- public/pages/workflows/import_workflow/import_workflow_modal.test.tsx
+++ public/pages/workflows/import_workflow/import_workflow_modal.test.tsx
@@ -7,15 +7,7 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import { Provider } from 'react-redux';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import configureStore from 'redux-mock-store';
-import thunk from 'redux-thunk';
-import {
-  INITIAL_OPENSEARCH_STATE,
-  INITIAL_WORKFLOWS_STATE,
-  INITIAL_PRESETS_STATE,
-  INITIAL_ML_STATE,
-  INITIAL_ERRORS_STATE,
-} from '../../../store';
+import { store } from '../../../store';
 import { ImportWorkflowModal } from './import_workflow_modal';
 
 jest.mock('../../../services', () => {
@@ -26,18 +18,8 @@ jest.mock('../../../services', () => {
   };
 });
 
-const mockConfigureStore = configureStore([thunk]);
-const initialState = {
-  opensearch: INITIAL_OPENSEARCH_STATE,
-  workflows: INITIAL_WORKFLOWS_STATE,
-  presets: INITIAL_PRESETS_STATE,
-  ml: INITIAL_ML_STATE,
-  errors: INITIAL_ERRORS_STATE,
-};
-
-const renderWithRouter = () => {
-  const store = mockConfigureStore(initialState);
-  return render(
+const renderWithRouter = () =>
+  render(
     <Provider store={store}>
       <Router>
         <Switch>
@@ -54,7 +36,6 @@ const renderWithRouter = () => {
       </Router>
     </Provider>
   );
-};
 
 describe('ImportWorkflowModal', () => {
   beforeEach(() => {
--- public/pages/workflows/workflows.test.tsx
+++ public/pages/workflows/workflows.test.tsx
@@ -14,15 +14,7 @@ import {
   Route,
   Switch,
 } from 'react-router-dom';
-import configureStore from 'redux-mock-store';
-import thunk from 'redux-thunk';
-import {
-  INITIAL_OPENSEARCH_STATE,
-  INITIAL_WORKFLOWS_STATE,
-  INITIAL_PRESETS_STATE,
-  INITIAL_ML_STATE,
-  INITIAL_ERRORS_STATE,
-} from '../../store';
+import { store } from '../../store';
 import { Workflows } from './workflows';
 
 jest.mock('../../services', () => {
@@ -33,33 +25,21 @@ jest.mock('../../services', () => {
   };
 });
 
-const mockConfigureStore = configureStore([thunk]);
-const initialState = {
-  opensearch: INITIAL_OPENSEARCH_STATE,
-  workflows: INITIAL_WORKFLOWS_STATE,
-  presets: INITIAL_PRESETS_STATE,
-  ml: INITIAL_ML_STATE,
-  errors: INITIAL_ERRORS_STATE,
-};
-
-const renderWithRouter = () => {
-  const store = mockConfigureStore(initialState);
-  return {
-    ...render(
-      <Provider store={store}>
-        <Router>
-          <Switch>
-            <Route
-              render={(props: RouteComponentProps) => (
-                <Workflows setActionMenu={jest.fn()} {...props} />
-              )}
-            />
-          </Switch>
-        </Router>
-      </Provider>
-    ),
-  };
-};
+const renderWithRouter = () => ({
+  ...render(
+    <Provider store={store}>
+      <Router>
+        <Switch>
+          <Route
+            render={(props: RouteComponentProps) => (
+              <Workflows setActionMenu={jest.fn()} {...props} />
+            )}
+          />
+        </Switch>
+      </Router>
+    </Provider>
+  ),
+});
 
 describe('Workflows', () => {
   beforeEach(() => {
