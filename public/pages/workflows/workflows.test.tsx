803924b19e785bb0ed99b119d60644ed48941797
Tyler Ohlsen <ohltyler@amazon.com>
2026-04-06 10:02:07 -0700

    Fix flaky tests by replacing singleton store with mock store
    Signed-off-by: Tyler Ohlsen <ohltyler@amazon.com>

--- public/pages/workflows/workflows.test.tsx
+++ public/pages/workflows/workflows.test.tsx
@@ -14,7 +14,15 @@ import {
   Route,
   Switch,
 } from 'react-router-dom';
-import { store } from '../../store';
+import configureStore from 'redux-mock-store';
+import thunk from 'redux-thunk';
+import {
+  INITIAL_OPENSEARCH_STATE,
+  INITIAL_WORKFLOWS_STATE,
+  INITIAL_PRESETS_STATE,
+  INITIAL_ML_STATE,
+  INITIAL_ERRORS_STATE,
+} from '../../store';
 import { Workflows } from './workflows';
 
 jest.mock('../../services', () => {
@@ -25,21 +33,33 @@ jest.mock('../../services', () => {
   };
 });
 
-const renderWithRouter = () => ({
-  ...render(
-    <Provider store={store}>
-      <Router>
-        <Switch>
-          <Route
-            render={(props: RouteComponentProps) => (
-              <Workflows setActionMenu={jest.fn()} {...props} />
-            )}
-          />
-        </Switch>
-      </Router>
-    </Provider>
-  ),
-});
+const mockConfigureStore = configureStore([thunk]);
+const initialState = {
+  opensearch: INITIAL_OPENSEARCH_STATE,
+  workflows: INITIAL_WORKFLOWS_STATE,
+  presets: INITIAL_PRESETS_STATE,
+  ml: INITIAL_ML_STATE,
+  errors: INITIAL_ERRORS_STATE,
+};
+
+const renderWithRouter = () => {
+  const store = mockConfigureStore(initialState);
+  return {
+    ...render(
+      <Provider store={store}>
+        <Router>
+          <Switch>
+            <Route
+              render={(props: RouteComponentProps) => (
+                <Workflows setActionMenu={jest.fn()} {...props} />
+              )}
+            />
+          </Switch>
+        </Router>
+      </Provider>
+    ),
+  };
+};
 
 describe('Workflows', () => {
   beforeEach(() => {
